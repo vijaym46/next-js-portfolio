a0a4101999b58e6eeed964d95b98754ee6053f73
vijay <vijaym4600@gmail.com>
2025-12-05 19:43:36 +0530

    First commit

---
+++ components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Spotlight } from '@/components/ui/spotlight'
+import { cn } from '@/lib/utils'
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+        <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
+        <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
+      </div>
+
+      <div className="absolute top-0 left-0 flex h-screen w-full bg-white dark:bg-black-100 items-center justify-center">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "bg-size-[100px_100px]",
+            "bg-[linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:bg-[linear-gradient(to_right,#262626_0.03%,transparent_1px),linear-gradient(to_bottom,#262626_0.03%,transparent_1px)]",
+          )}
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+      
+      <div className="flex justify-center relative my-20 z-10">
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest text-sm text-center text-blue-100 max-w-80'>
+            Dynamic Web Magic with React.js
+          </h2>
+          <TextGenerateEffect 
+            className='text-center text-[40px] md:text-4xl lg:text-5xl'
+            words='Transforming Concepts into Seamless User Experiences'
+            // duration={1}
+            // filter={false}
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Vijay, a React.js Developer based in India
+          </p>
+          <a href="#about">
+            <MagicButton 
+              title="Show my work" 
+              icon={<FaLocationArrow />}
+              position='right'
+            />
+          </a>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
